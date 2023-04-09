567242c71ba47c171d8d70a912969698efc9c57f
Ebenezer <akhonyaebenezer@gmail.com>
2023-04-09 14:57:27 +0300

    [Enhancement]: Refactor the script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -21,52 +21,23 @@ server {
 	}
 }	
 "
-function install_nginx(){
-    #start with installing nginx( if it's not already installed)
-    if ! [[ "$(which nginx)" ]]; then
-        #install nginx
-        sudo apt-get -y update
-        sudo apt-get -y install nginx
-    fi
-}
-#creating folders if they dont exist
-function create_folders(){
-    if ! [[ -d "/data/web_static/releases/test/" ]]; then
-        mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
-    fi
-    chown -R ubuntu:ubuntu /data/
-}
+if ! [[ "$(which nginx)" ]]; then
+    sudo apt-get -y update
+    sudo apt-get install nginx
 
-#creating fake HTML files
-function create_fake_html(){
-    if ! [[ -s "/data/web_static/releases/test/index.html" ]]; then
-        echo -e "$HTML_CONTENT" > /data/web_static/releases/test/index.html
-    fi
-}
+if ! [[ -d /data/web_static/releases/test/ ]]; then
+    sudo mkdir -p /data/web_static/releases/test/
 
-#creating a symlink ( if it doen't already exist)
-function create_symlink(){
-    if [[ -L /data/web_static/current ]]; then
+sudo chown -R ubuntu:ubuntu /data/
+
+echo -e "$HTML_CONTENT" > /data/web_static/releases/test/index.html
+
+if [ -L /data/web_static/current ]; then
     rm /data/web_static/current
-    fi
+fi
 
-    ln -s /data/web_static/releases/test/ /data/web_static/current
-}
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-#configure nginx
-function configure_nginx(){
-echo  -e "$NGINX_CONFIG" > /etc/nginx/sites-available/default
-}
+echo -e "$NGINX_CONFIG" > /etc/nginx/sites-enabled/default
 
-function restart_nginx() {
-    if [[ "$(pgrep nginx)" ]]; then
-        sudo service nginx restart
-    else
-        sudo service nginx start
-    fi
-}
-install_nginx;
-create_folders;
-create_fake_html;
-create_symlink;
-restart_nginx;
+sudo service nginx restart
