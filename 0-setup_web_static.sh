84922c15333923579adccc855bdda7265258c1c9
Ebenezer <akhonyaebenezer@gmail.com>
2023-04-09 18:32:40 +0300

    [Chore]: Add task one

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,25 @@
 #!/usr/bin/env bash
 #set things up for deployment
-HTML_CONTENT=\
-"
-<html>
-    <head>
-        <title>A dumy page </title>
-    </head>
-    <body>
-        Hi Human, good to see you here
-    </body>
-</html>
-"
-# install nginx if not present
-if ! [[ "$(which nginx)" ]]; then
-	sudo apt-get update -y  && \
-	     sudo apt-get install -y nginx
+
+if [ ! -x /usr/sbin/nginx ]; then
+	sudo apt-get update -y -qq && \
+	    sudo apt-get install -y nginx
 fi
 
 
+# make dirs...
+sudo mkdir -p /data/web_static/releases/test  /data/web_static/shared/
 
-# Create directories...
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared/
 
-# setup some fake content
-echo -e "$HTML_CONTENT" > data/web_static/releases/test/index.html
+echo "<h1> I couldn't center a div </h1>" | sudo dd status=none of=/data/web_static/releases/test/index.html
 
 # create sym link
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-# changing ownership of the /data/ dir to
 sudo chown -R ubuntu:ubuntu /data/
 
-# backup default server config file
-sudo cp /etc/nginx/sites-enabled/default /etc/nginx/conf.d/backup.bck
+
+sudo cp /etc/nginx/sites-enabled/default /etc/nginx/nginx-sites-enabled_default.bck
 
 
 sudo sed -i '37i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
