81902ce71d0650ebef334e2d685b6858864fc29c
Ebenezer <akhonyaebenezer@gmail.com>
2023-04-21 21:09:52 +0300

    [Chore]: Add / route

---
+++ web_flask/0-route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Simple flask server with only one end point
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_holberton():
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
